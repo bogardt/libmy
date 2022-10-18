274977dfea07101d90d9ca5764980dffe0cc06a8
bogardt <thom.bogard@gmail.com>
2022-10-18 19:11:45 +0200

    fixed strcat & strncat tests

--- tests/test_my_strcat.c
+++ tests/test_my_strcat.c
@@ -1,10 +1,46 @@
 #include "mytest.h"
+#include <string.h>
+#include <stdio.h>
 
-START_TEST(test_my_strcat_should_success)
+START_TEST(test_my_strcat_should_success_level_1)
 {
-    ck_assert_str_eq(my_strcat("tata", "toto"), "tatatoto");
-    ck_assert_str_eq(my_strcat("tata", "totosad12313"), "tatatotosad12313");
-    ck_assert_str_eq(my_strcat("toto teuf", " encore un weekend"), "toto teuf encore un weekend");
+    char *s1 = "tata";
+    char *s2 = "toto";
+
+    int length = strlen(s1) + strlen(s2);
+    char *res = (char *)malloc(sizeof(char) * length);
+    
+    strcpy(res, s1);
+
+    ck_assert_str_eq(my_strcat(res, s2), "tatatoto");
+}
+END_TEST
+
+START_TEST(test_my_strcat_should_success_level_2)
+{
+    char *s1 = "tata";
+    char *s2 = "totosad12313";
+
+    int length = strlen(s1) + strlen(s2);
+    char *res = (char *)malloc(sizeof(char) * length);
+    
+    strcpy(res, s1);
+
+    ck_assert_str_eq(my_strcat(res, s2), "tatatotosad12313");
+}
+END_TEST
+
+START_TEST(test_my_strcat_should_success_level_3)
+{
+    char *s1 = "toto teuf";
+    char *s2 = " encore un weekend";
+
+    int length = strlen(s1) + strlen(s2);
+    char *res = (char *)malloc(sizeof(char) * length);
+    
+    strcpy(res, s1);
+
+    ck_assert_str_eq(my_strcat(res, s2), "toto teuf encore un weekend");
 }
 END_TEST
 
@@ -14,7 +50,9 @@ Suite *my_strcat_suite(void)
 
     /* Core test case */
     TCase *tc_core = tcase_create("Core");
-    tcase_add_test(tc_core, test_my_strcat_should_success);
+    tcase_add_test(tc_core, test_my_strcat_should_success_level_1);
+    tcase_add_test(tc_core, test_my_strcat_should_success_level_2);
+    tcase_add_test(tc_core, test_my_strcat_should_success_level_3);
     suite_add_tcase(s, tc_core);
 
     return s;
--- tests/test_my_strncat.c
+++ tests/test_my_strncat.c
@@ -1,8 +1,46 @@
 #include "mytest.h"
+#include <string.h>
+#include <stdio.h>
 
-START_TEST(test_my_strncat_should_success)
+START_TEST(test_my_strncat_should_success_level_1)
 {
-    ck_assert_str_eq(my_strncat("tata", "toto", 2), "tatato");
+    char *s1 = "tata";
+    char *s2 = "toto";
+
+    int length = strlen(s1) + strlen(s2);
+    char *res = (char *)malloc(sizeof(char) * length);
+    
+    strcpy(res, s1);
+
+    ck_assert_str_eq(my_strncat(res, s2, 1), "tatat");
+}
+END_TEST
+
+START_TEST(test_my_strncat_should_success_level_2)
+{
+    char *s1 = "tata";
+    char *s2 = "totosad12313";
+
+    int length = strlen(s1) + strlen(s2);
+    char *res = (char *)malloc(sizeof(char) * length);
+    
+    strcpy(res, s1);
+
+    ck_assert_str_eq(my_strncat(res, s2, 3), "tatatot");
+}
+END_TEST
+
+START_TEST(test_my_strncat_should_success_level_3)
+{
+    char *s1 = "toto teuf";
+    char *s2 = " encore un weekend";
+
+    int length = strlen(s1) + strlen(s2);
+    char *res = (char *)malloc(sizeof(char) * length);
+    
+    strcpy(res, s1);
+
+    ck_assert_str_eq(my_strncat(res, s2, 5), "toto teuf enco");
 }
 END_TEST
 
@@ -12,7 +50,9 @@ Suite *my_strncat_suite(void)
 
     /* Core test case */
     TCase *tc_core = tcase_create("Core");
-    tcase_add_test(tc_core, test_my_strncat_should_success);
+    tcase_add_test(tc_core, test_my_strncat_should_success_level_1);
+    tcase_add_test(tc_core, test_my_strncat_should_success_level_2);
+    tcase_add_test(tc_core, test_my_strncat_should_success_level_3);
     suite_add_tcase(s, tc_core);
 
     return s;
